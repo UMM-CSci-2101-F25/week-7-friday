ac45eeb31940946dd334e9fa4578bcddc4aed509
K.K. Lamberty <lamberty@morris.umn.edu>
2025-10-04 13:42:51 -0500

    Demonstrate use of Stack in main

--- src/main/java/umm2101/JustMain.java
+++ src/main/java/umm2101/JustMain.java
@@ -3,6 +3,14 @@ package umm2101;
 public class JustMain {
 
     public static void main(String[] args) {
+        Stack<Integer> myStack = new AStack<>();
+        myStack.push(23);
+        myStack.push(34);
+        myStack.push(45);
+
+        for ( ; !myStack.isEmpty(); myStack.pop()){
+            System.out.println(myStack.topValue());
+        }
         System.out.println("This is coming from the class JustMain when you run the main method.\nUse this to exercise your code and help you understand it.");
     }
 
